905b5bdd14732198e9fe22de95e71fcef07e378f
liuxue <1424449861@qq.com>
2020-07-21 19:59:58 +0800

    11

--- src/com/xt/ftest/work/Test.java
+++ src/com/xt/ftest/work/Test.java
@@ -2,6 +2,6 @@ package com.xt.ftest.work;
 
 public class Test {
      public static void main(String[] args) {
-		System.out.println("this is a test");
+		System.out.println("this is a test page");
 	}
 }
